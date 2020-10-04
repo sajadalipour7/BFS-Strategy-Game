0eba475282bffaed0a6d5c89d77d3daaea8f6b6d
Sajad <sajadalipour@aut.ac.ir>
2020-10-04 14:55:42 +0330

    Basics

---
+++ BFS Strategy Game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting:" + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
